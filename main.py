e7d0dc4d396ba9796145bdbcde9c0a5e91a693ff
Brunoczbr <brunoczbr@gmail.com>
2025-04-21 11:32:42 -0300

    definição da função de busca

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from fastapi.staticfiles import StaticFiles
 import requests
 
 app = FastAPI(title="Buscador de Músicas Deezer")
+
+#função para busca de músicas
+@app.get("/buscar")
+def buscar_musica(q: str):
+    url = f"https://api.deezer.com/search?q={q}"
+    resposta = requests.get(url)
